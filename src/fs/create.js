91d70620474c12f487c8978f0ed8ee47613b716d
andron13 <andron13@gmail.com>
2022-12-05 09:34:17 +0100

    feat: add function fsPromises.cp (Experimental)

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,6 +1,6 @@
 import {Buffer} from 'node:buffer';
 import fs from "fs";
-import * as url from 'url';
+import url from "url";
 
 const successText = "I am fresh and young";
 const data = new Uint8Array(Buffer.from(successText));
@@ -10,13 +10,13 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const pathToFile = __dirname + "/files/" + file;
 
 const create = async () => {
-    try {
-        await fs.promises.writeFile(pathToFile, data, {
-            flag: 'wx'
-        });
-    } catch (err) {
-        throw new Error(errorText)
-    }
+	try {
+		await fs.promises.writeFile(pathToFile, data, {
+			flag: 'wx'
+		});
+	} catch (err) {
+		throw new Error(errorText)
+	}
 };
 
 await create();
